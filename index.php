1f3c105982c477b8b8bff876f032ba54f37432d0
Stefan Bohacek <stefan@fourtonfish.com>
2021-02-24 12:16:26 -0500

    Added logging to debug an issue with Twitter API.

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
  * Plugin Name: TEmbeds
  * Plugin URI: https://github.com/fourtonfish/tweet-embeds-wordpress-plugin
  * Description: Embed Tweets without compromising your users' privacy and your site's performance.
- * Version: 1.0.8
+ * Version: 1.0.9
  * Author: fourtonfish
+ * Text Domain: tembeds
  *
  * @package ftf-alt-embed-tweet
  */
@@ -65,9 +66,9 @@ class FTF_Alt_Embed_Tweet {
 
             $response = wp_remote_get( $api_endpoint, $args );
         } else {
-            // error_log( print_r( array(
-            //     'token errors' => $token->errors
-            // ), true ) );
+            error_log( print_r( array(
+                'token errors' => $token->errors
+            ), true ) );
             $this->update_error_log( $token->errors );
         }
 
@@ -180,6 +181,11 @@ class FTF_Alt_Embed_Tweet {
                     );
 
                     $response = self::call_twitter_api(  'tweets?' . str_replace( '%2C', ',', http_build_query( $post_fields ) ) );
+
+                    error_log( print_r( array(
+                        'Twitter API response' => $response
+                    ), true ) );
+
                     $response_array = json_decode( rtrim($response, "\0") );
                     $tweet_data = array();
 
